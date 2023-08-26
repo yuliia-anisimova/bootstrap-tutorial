9cc8746289943d7fba6ad4fc5c2511124303dab1
Yuliia Anisimova <anisimova.yuliia01@gmail.com>
2023-08-26 09:43:38 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+// Import only the Bootstrap components we need
+// import { Popover } from 'bootstrap';
+
+// Create an example popover
+// document.querySelectorAll('[data-bs-toggle="popover"]')
+//   .forEach(popover => {
+//     new Popover(popover)
+//   })
