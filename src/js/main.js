de3045a7b8a756a1ff70675f93ecfd672e3f3725
Yuliia Anisimova <anisimova.yuliia01@gmail.com>
2023-08-26 22:55:59 +0300

    Add modal for enrollment

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,2 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
-// Import only the Bootstrap components we need
-// import { Popover } from 'bootstrap';
-
-// Create an example popover
-// document.querySelectorAll('[data-bs-toggle="popover"]')
-//   .forEach(popover => {
-//     new Popover(popover)
-//   })
